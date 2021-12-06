0828c7fa7cba433236a17cfc8046aead4cd19e0e
bluestarburst <hargreaves.bryant@gmail.com>
2021-12-06 09:08:13 -0600

    server

--- client/webrtc.js
+++ client/webrtc.js
@@ -66,7 +66,7 @@ export default class webrtc {
 
     async connect() {
         return new Promise(function (resolve, reject) {
-            var server = new WebSocket("wss://47.184.194.78:26950/");
+            var server = new WebSocket("wss://stuff.bryanthargreave.repl.co:26950/");
             server.onopen = function () {
                 resolve(server);
             };
